4d68f7bf0e3b49fcd8b4d8bb6c232853f2e36be6
LucasMdosAnjos <lucasmon@live.com>
2019-04-21 16:10:29 -0300

    loja_virtual v2

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,26 @@ class MyApp extends StatelessWidget {
       theme: new ThemeData(
         primarySwatch: Colors.blue
       ),
+      home: Home(),
+    );
+  }
+}
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Loja Virtual'),
+        centerTitle: true,
+      ),
+      body: Center(
+          child: Text('Hello!')
+      ),
     );
   }
 }
